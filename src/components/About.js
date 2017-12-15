9ea44d0707d9635edcdb9d55860b931d0b37c7b0
Saada Mickael <mickysaada@gmail.com>
2017-12-15 10:36:09 +0100

    feat(about): make about page responsive

--- src/components/About.js
+++ src/components/About.js
@@ -13,6 +13,12 @@ class About extends Component {
     return {
       divider: {
         marginBottom: "1em"
+      },
+      CEOletter: {
+        maxWidth: "50rem",
+        display: "flex",
+        flexDirection: "column",
+        justifyContent: "center"
       }
     };
   }
@@ -25,41 +31,44 @@ class About extends Component {
         <div className="body">
           <h1>A propos</h1>
           <Divider style={this.styles().divider} />
-
-          <p>Cher Docteur,</p>
-          <p>
-            Nous sommes heureux de vous présenter notre nouveau concept store,
-            la MEGA Dental ART GALLERY.
-          </p>
-          <p>
-            Nous avons voulu, à travers cette galerie, participer à la diffusion
-            et à la promotion d'oeuvres sélectionnées parmi les meilleurs
-            artistes contemporains tels que: Richard Orlinski, Jonone, Combas,
-            Nebay, Belmondo, Pasqua, Gambino, Patrick Rubinstein, Arman...
-          </p>
-          <p>
-            L'exposition d'oeuvres d'art dans votre cabinet dentaire vous
-            permettra, sans aucun douten de valoriser et de donner un supplément
-            d'âme à votre espace de travail. Vous pouvez ainsi créer un nouveau
-            lien culturel, à la fois avec vos patients et vos collaborateurs.
-          </p>
-          <p>
-            Vous contribuerez à la diffusion de l'art au plus grand nombre tout
-            en embelissant votre environnement et en lui donnant une image
-            innovante.
-          </p>
-          <p>
-            Un oeuvre d'art a, bien souvent, cette capacité à faire naître des
-            émotions et à réduire le stress.
-          </p>
-          <p>
-            L'équipe MEGA Dental se tient à votre disposition pour vous apporter
-            les explications et les conseils que vous souhaiteriez. N'hésitez
-            pas à nous contacter.
-          </p>
-          <p>Bien chaleureusement.</p>
-          <p>Joël Drai,</p>
-          <p>Président.</p>
+          <div style={this.styles().CEOletter}>
+            <p>Cher Docteur,</p>
+            <p>
+              Nous sommes heureux de vous présenter notre nouveau concept store,
+              la MEGA Dental ART GALLERY.
+            </p>
+            <p>
+              Nous avons voulu, à travers cette galerie, participer à la
+              diffusion et à la promotion d'oeuvres sélectionnées parmi les
+              meilleurs artistes contemporains tels que: Richard Orlinski,
+              Jonone, Combas, Nebay, Belmondo, Pasqua, Gambino, Patrick
+              Rubinstein, Arman...
+            </p>
+            <p>
+              L'exposition d'oeuvres d'art dans votre cabinet dentaire vous
+              permettra, sans aucun douten de valoriser et de donner un
+              supplément d'âme à votre espace de travail. Vous pouvez ainsi
+              créer un nouveau lien culturel, à la fois avec vos patients et vos
+              collaborateurs.
+            </p>
+            <p>
+              Vous contribuerez à la diffusion de l'art au plus grand nombre
+              tout en embelissant votre environnement et en lui donnant une
+              image innovante.
+            </p>
+            <p>
+              Un oeuvre d'art a, bien souvent, cette capacité à faire naître des
+              émotions et à réduire le stress.
+            </p>
+            <p>
+              L'équipe MEGA Dental se tient à votre disposition pour vous
+              apporter les explications et les conseils que vous souhaiteriez.
+              N'hésitez pas à nous contacter.
+            </p>
+            <p>Bien chaleureusement.</p>
+            <p>Joël Drai,</p>
+            <p>Président.</p>
+          </div>
         </div>
 
         <Footer />
