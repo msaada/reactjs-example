af38d8a5afe399ea398de2221286d72d0cc8c9b3
Saada Mickael <mickysaada@gmail.com>
2017-12-19 17:23:09 +0200

    feat(app): integrate google analytics

--- src/index.js
+++ src/index.js
@@ -10,7 +10,8 @@ import registerServiceWorker from "./javascript/registerServiceWorker";
 import injectTapEventPlugin from "react-tap-event-plugin";
 import { MuiThemeProvider, createMuiTheme } from "material-ui/styles";
 import orangeA700 from "material-ui/colors/orange";
-
+import ReactGA from "react-ga";
+ReactGA.initialize("UA-111415811-1");
 // Needed for onTouchTap
 // http://stackoverflow.com/a/34015469/988941
 injectTapEventPlugin();
