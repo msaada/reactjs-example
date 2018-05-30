9a4d021c8b4bca39c141a1ffea3a61a79833dfb1
Mickaël Saada <mickael.saada@fluo.com>
2018-05-30 08:45:39 +0200

    refactor(categories): add types

--- src/datas/categories.js
+++ src/datas/categories.js
@@ -1,97 +1,104 @@
 //@flow
 
-export const categories = {
+type Categories = {
+  [string]: {
+    id: string,
+    url: string,
+  },
+};
+
+export const categories: Categories = {
   sculpture: {
-    id: "-KwfxjWE2pynkWGi_9JH",
-    url: "sculpture"
+    id: '-KwfxjWE2pynkWGi_9JH',
+    url: 'sculpture',
   },
   peinture: {
-    id: "-KxcQHV437QWxoKaMwVH",
-    url: "peinture"
+    id: '-KxcQHV437QWxoKaMwVH',
+    url: 'peinture',
   },
   photographie: {
-    id: "-Kwfxk_dy1aWCYVBAGXx",
-    url: "photographie"
+    id: '-Kwfxk_dy1aWCYVBAGXx',
+    url: 'photographie',
   },
   acrylique: {
-    id: "-KxcP8I0YWCqfFTC4GoL",
-    url: "acrylique_aerosol"
+    id: '-KxcP8I0YWCqfFTC4GoL',
+    url: 'acrylique_aerosol',
   },
   digigraphie: {
-    id: "-KxcPAhyooHriU55Oiyb",
-    url: "digigraphie"
+    id: '-KxcPAhyooHriU55Oiyb',
+    url: 'digigraphie',
   },
   gravure: {
-    id: "-KxcPC9OBaB9FtXhlgYI",
-    url: "gravure"
+    id: '-KxcPC9OBaB9FtXhlgYI',
+    url: 'gravure',
   },
   huile_sur_toile: {
-    id: "-KxcPGo8GFSoqj2IVaTx",
-    url: "huile_sur_toile"
+    id: '-KxcPGo8GFSoqj2IVaTx',
+    url: 'huile_sur_toile',
   },
   linogravure: {
-    id: "-KxcPJDI8BAD5boeedeW",
-    url: "linogravure"
+    id: '-KxcPJDI8BAD5boeedeW',
+    url: 'linogravure',
   },
   meuble: {
-    id: "-KxcPK1CM8UIBP6jrTw9",
-    url: "meuble"
+    id: '-KxcPK1CM8UIBP6jrTw9',
+    url: 'meuble',
   },
   mobilier_accessoire: {
-    id: "-KxcPMZRsHbkxZ07aoCG",
-    url: "mobilier_accessoire"
+    id: '-KxcPMZRsHbkxZ07aoCG',
+    url: 'mobilier_accessoire',
   },
   montage_photographique: {
-    id: "-KxcPOI4hP8yilm1VKsE",
-    url: "montage_photographique"
+    id: '-KxcPOI4hP8yilm1VKsE',
+    url: 'montage_photographique',
   },
   peinture_carton: {
-    id: "-KxcPSbHvt4PQqXmubWB",
-    url: "peinture_carton"
+    id: '-KxcPSbHvt4PQqXmubWB',
+    url: 'peinture_carton',
   },
   peinture_a_l_huile: {
-    id: "-KxcPUpcJ5ro8R_D5Mah",
-    url: "peinture_a_l_huile"
+    id: '-KxcPUpcJ5ro8R_D5Mah',
+    url: 'peinture_a_l_huile',
   },
 
   photomontage: {
-    id: "-Ky84IseXbFYp_tN_OAO",
-    url: "photomontage"
+    id: '-Ky84IseXbFYp_tN_OAO',
+    url: 'photomontage',
   },
   sculpture_peinture: {
-    id: "-Ky84M0r721g5uePvvJQ",
-    url: "sculpture_peinture"
+    id: '-Ky84M0r721g5uePvvJQ',
+    url: 'sculpture_peinture',
   },
   sculpture_resine: {
-    id: "-Ky84N_F2tR6wFHl-_Eu",
-    url: "sculpture_resine"
+    id: '-Ky84N_F2tR6wFHl-_Eu',
+    url: 'sculpture_resine',
   },
   serigraphie: {
-    id: "-Ky84OnfHX_nY-mAI2Nq",
-    url: "serigraphie"
+    id: '-Ky84OnfHX_nY-mAI2Nq',
+    url: 'serigraphie',
   },
   tableau_acrylique: {
-    id: "-Ky84RV88gXxRHw318ry",
-    url: "acrylique_sur_toile"
+    id: '-Ky84RV88gXxRHw318ry',
+    url: 'acrylique_sur_toile',
   },
   technique_numerique: {
-    id: "-Ky84ThkB9Jmi48X-rM5",
-    url: "technique_numerique"
+    id: '-Ky84ThkB9Jmi48X-rM5',
+    url: 'technique_numerique',
   },
   toile: {
-    id: "-Ky84VItpZyRoVWAR-Bd",
-    url: "toile"
+    id: '-Ky84VItpZyRoVWAR-Bd',
+    url: 'toile',
   },
   oeuvre_dangle: {
-    id: "-Ky84WOd_KU7OGvIXY8i",
-    url: "oeuvre_dangle"
+    id: '-Ky84WOd_KU7OGvIXY8i',
+    url: 'oeuvre_dangle',
   },
   oeuvre_sur_toile: {
-    id: "-Ky84_9XiqhMHr-8RzIQ",
-    url: "oeuvre_sur_toile"
+    id: '-Ky84_9XiqhMHr-8RzIQ',
+    url: 'oeuvre_sur_toile',
   },
   oeuvre_murale_destructuree: {
-    id: "-KzXNjkLZaqceQ8gkgtR",
-    url: "oeuvre_murale_destructuree"
-  }
+    id: '-KzXNjkLZaqceQ8gkgtR',
+    url: 'oeuvre_murale_destructuree',
+  },
 };
